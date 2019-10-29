81214c9435573909989ba2814a706321af4a22eb
bhaskararao-nyros <bhaskararao.nyros@gmail.com>
2019-10-29 22:47:23 +0530

    Server js changes

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
-// const api = require('./routes/api');
-// const adminRoutes = require('./routes/admin-routes');
+const api = require('./routes/api');
+const adminRoutes = require('./routes/admin-routes');
 
 const app = express();
 
 // const db = 'mongodb://localhost/mean_auth';
+const db = process.env.MONGODB_URI || 'mongodb://bhaskarDB:Bhaskar1@db@ds141248.mlab.com:41248/heroku_hjw9t9l8';
 
-// mongoose.connect(db, err => {
-// 	if (err) {
-// 		console.error('Error!' + err);
-// 	} else {
-// 		console.log('Connected to mongoDB');
-// 	}
-// })
+mongoose.connect(db, err => {
+	if (err) {
+		console.error('Error!' + err);
+	} else {
+		console.log('Connected to mongoDB');
+	}
+})
 
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use('/api', api);
-// app.use('/admin', adminRoutes);
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', api);
+app.use('/admin', adminRoutes);
 
 app.get('/', (req, res)=> {
 	res.send("Hello from server");
